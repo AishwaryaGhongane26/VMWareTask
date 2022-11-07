eab1b3d900e0aa9bd580cadb09c9637d24da4024
AishwaryaGhongane26 <aishwarya.ghongane@gmail.com>
2022-11-07 10:27:05 +0000

    commit

--- DisplayNumberOfBooksByAuthor/force-app/main/default/lwc/displayNumberOfBooks/displayNumberOfBooks.js
+++ DisplayNumberOfBooksByAuthor/force-app/main/default/lwc/displayNumberOfBooks/displayNumberOfBooks.js
@@ -8,12 +8,12 @@ const Columns = [
    
     ]// colum for datatable
     //why wirev decorator?  wire is recative in nature  when ever their is change in org data it will automaticaaly reflect in lwc .
-export default class Releted_Contact extends LightningElement {
+export default class Related_Author extends LightningElement {
    @api recordId;   // give reacordid of account
    publishHouse;
     error;
     cols=Columns; //if the Apex method takes a string parameter, don’t pass a string directly. Instead, pass an object that contains a property whose value is a string.
-   @wire(getPublishingHouse,{val: '$recordId'})wiredContacts({ error, data }) {
+   @wire(getPublishingHouse,{val: '$recordId'})wiredAuthor({ error, data }) {
     if (data) {
         this.publishHouse = data;
         this.error = undefined;
@@ -25,9 +25,6 @@ export default class Releted_Contact extends LightningElement {
 rowData.Author__c_Author_Name__c = row.Author__r.Author_Name__c;
 rowData.Author__c_Number_of_Books_Published__c = row.Author__r.Number_of_Books_Published__c;
 
-// if (row.Author__c_First_Name__c) {
-//     rowData.Author__c_Number_of_Books_Published__c = row.Author__r.Number_of_Books_Published__c;
-// }
 currentData.push(rowData);
 
 });
